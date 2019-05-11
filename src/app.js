e7559666c7ac35d2f1afd9d38dbba6683b79ca77
Samantha Ambroise <ilkigin@gmail.com>
2019-05-11 14:45:55 -0400

    added tests

--- src/app.js
+++ src/app.js
@@ -16,14 +16,42 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.get('bookmarks', (req, res, next) => {
+app.get('/bookmarks', (req, res, next) => {
     const knexInstance = req.app.get('db')
     BookmarksService.getAllBookmarks(knexInstance)
         .then(bookmarks => {
-            res.json(bookmarks)
+            res.json(bookmarks.map(bookmark => ({
+                id: bookmark.id,
+                title: bookmark.title,
+                url: bookmark.url,
+                description: bookmark.description,
+                rating: bookmark.rating,
+       })))
         })
         .catch(next)
 })
+
+app.get('/bookmarks/:bookmark_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    BookmarksService.getBookmarkById(
+        req.app.get('db'),
+        req.params.bookmark_id
+    )
+        .then(bookmark => {
+            if(!bookmark) {
+                return res.status(404).json({ error: { message: `Bookmark doesn't exist` }})
+            }
+            res.json({
+                id: bookmark.id,
+                title: bookmark.title,
+                url: bookmark.url,
+                description: bookmark.description,
+                rating: bookmark.rating,
+            })
+        })
+        .catch(next)
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,76 @@
+function makeBookmarksArray(){
+    return [
+        {
+            id: 1,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 2,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 3,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 4,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 5,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 6,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 7,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 8,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 9,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        },
+        {
+            id: 10,
+            title: 'first bookmark',
+            url: 'someplace.com',
+            description: 'Lorem some stuff',
+            rating: 1,
+        }
+    ]
+}
+
+module.exports = {makeBookmarksArray}
